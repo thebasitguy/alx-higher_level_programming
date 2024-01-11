320a55f0303782611d6ad79c44cfce472dd17a06
thebasitguy <mailbasitgh@gmail.com>
2024-01-10 21:09:59 -0800

    errors fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
         return 0
     mapping_data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_values = [mapping_data[x] for x in roman_string] + [0]
     result = 0
-
-    for i in range(len(int_values) - 1):
-        if int_values[i] >= int_values[i+1]:
-            result += int_values[i]
+    for i in range(len(roman_string)):
+        if i > 0 and mapping_data[roman_string[i]] > mapping_data[roman_string[i - 1]]:
+            result += mapping_data[roman_string[i]] - 2 * \
+                        mapping_data[roman_string[i - 1]]
         else:
-            result -= int_values[i]
-
+            result += mapping_data[roman_string[i]]
     return result
