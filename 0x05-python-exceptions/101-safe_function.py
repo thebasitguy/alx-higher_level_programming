780e21062429daf6b13faecd36d2839dbb7720ab
thebasitguy <mailbasitgh@gmail.com>
2024-01-22 20:09:10 -0800

    errors fixed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+from __future__ import print_function
+import sys
 
 def safe_function(fct, *args):
-
-	"""Executes a function safely"""
-
-    import sys
-
-	try:
-		ret = fct(*args)
-        return ret
+    try:
+        result = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
+    else:
+        return result
