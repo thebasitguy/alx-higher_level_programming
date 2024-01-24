e164bbbe0f04372173c6c7a1aa0c41138ddeeb56
thebasitguy <mailbasitgh@gmail.com>
2024-01-23 18:48:43 -0800

    a MagicClass matching exactly a bytecode provided by Holberton

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Defines a MagicClass matching exactly a bytecode provided by Holberton"""
+
+import math
+
+
+class MagicClass:
+    """Circle representation"""
+
+    def __init__(self, radius=0):
+        
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Returns area of the MagicClass"""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Returns circumference of the MagicClass"""
+        return (2 * math.pi * self.__radius)
