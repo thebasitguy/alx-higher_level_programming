a7e95b4cf5546f5338e6f5a2894c30298f2bde45
thebasitguy <mailbasitgh@gmail.com>
2024-02-06 18:19:15 -0800

    File 2 checked

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that appends a string at the end of a text file"""
+"""Function that appends string at the end of a text file"""
 
 
 def append_write(filename="", text=""):
@@ -12,4 +12,4 @@ def append_write(filename="", text=""):
         Number of characters appended
     """
     with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text) 
+        return f.write(text)
