94b3748b56858151827e712faf7bf95d17bd209b
thebasitguy <mailbasitgh@gmail.com>
2024-04-16 11:41:26 -0700

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-Prints all City objects and their State in a database.
+Lists all City objects and their State in a database.
 '''
 
 import sys
@@ -12,19 +12,18 @@ from model_state import Base, State
 from model_city import City
 
 
-if __name__ == "__main__":
-
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for q in session.query(State.name, City.id, City.name).filter(
-            State.id == City.state_id).order_by(City.id):
-        print("{:s}: ({:d}) {:s}".format(q[0], q[1], q[2]))
-
-    session.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        State.cities = relationship('City', back_populates='state')
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(City).order_by(City.id.asc()).all()
+        for row in result:
+            print('{}: ({}) {}'.format(row.state.name, row.id, row.name))
