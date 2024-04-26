af996a0aee431ab1636d9fa818b846039e014c3e
thebasitguy <mailbasitgh@gmail.com>
2024-04-26 09:08:23 -0700

    3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script takes in a URL,sends a request to
+the URL and displays the body of the response."""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(url) as response:
+            url_res = response.read()
+            print(url_res.decode('utf-8'))
+
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
