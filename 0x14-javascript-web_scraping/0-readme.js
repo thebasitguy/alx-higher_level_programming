39434bc5a2d1e089441e8811ad96d4ac1b16fa6d
thebasitguy <mailbasitgh@gmail.com>
2024-05-21 18:34:55 -0700

    errors checked

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -12,7 +12,8 @@ fs.readFile(process.argv[2], 'utf8', function (error, content) {
     console.error('Error reading the file:', error);
 
   } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
+    // If the file is read successfully, the 'content' parameter will contain
+    // the contents of the file as a string.
     console.log(content);
   }
 });
